8124773294e32c35720c644cea0a00d5bdbe2e63
miniduharshana <miniduharshanaherath@gmail.com>
2023-09-01 21:15:28 +0530

    edited by samankumara

---
+++ tests/test_cal.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+# Add the parent directory (project root) to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Now you can import your modules using absolute imports
+from calculator import add
+
+def test_add_positive_numbers():
+    assert add(2, 3) == 5
+
+def test_add_zero():
+    assert add(0, 0) == 0
+
+def test_add_negative_numbers():
+    assert add(-2, -3) == -6
